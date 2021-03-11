84502d8ec85f287ee62efabe8a9bd00710ae242a
Marcin Lenkowski <marcin@lenkowski.net>
2021-03-11 22:33:29 +0100

    check .env

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -33,6 +33,25 @@ class TestCase extends \EscolaLms\Core\Tests\TestCase
     {
         $app['config']->set('auth.providers.users.model', User::class);
         $app['config']->set('passport.client_uuids', true);
+        $app['config']->set('database.default', env('DB_CONNECTION'));
+        $app['config']->set('database.connections.' . env('DB_CONNECTION'), [
+            'driver' => env('DB_CONNECTION'),
+            'host' => env('DB_HOST'),
+            'port' => env('DB_PORT'),
+            'database' => env('DB_DATABASE'),
+            'password' => env('DB_PASSWORD'),
+            'username' => env('DB_USERNAME'),
+            'prefix' => '',
+        ]);
+        dd([
+            'driver' => env('DB_CONNECTION'),
+            'host' => env('DB_HOST'),
+            'port' => env('DB_PORT'),
+            'database' => env('DB_DATABASE'),
+            'password' => env('DB_PASSWORD'),
+            'username' => env('DB_USERNAME'),
+            'prefix' => '',
+        ]);
         $app['config']->set('passport.storage.database.connection', env('DB_CONNECTION'));
     }
 }
