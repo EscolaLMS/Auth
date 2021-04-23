c7ef194b24ccc33951e465d0d71630fedb58103b
Paweł Chołaj <pawel.cholaj@escola.pl>
2021-04-23 18:06:32 +0200

    More tests

--- src/Http/Requests/Admin/UserUpdateRequest.php
+++ src/Http/Requests/Admin/UserUpdateRequest.php
@@ -2,16 +2,33 @@
 
 namespace EscolaLms\Auth\Http\Requests\Admin;
 
+use EscolaLms\Auth\Models\User;
+use Illuminate\Validation\Rule;
+
 class UserUpdateRequest extends AbstractUserIdInRouteRequest
 {
     public function rules()
     {
         $rules = [
-            'first_name' => ['sometimes', 'string', 'max:255'],
-            'last_name' => ['sometimes', 'string', 'max:255'],
-            'email' => ['sometimes', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['sometimes', 'string', 'min:6'],
+            'first_name' => [$this->requiredIfPut(), 'string', 'max:255'],
+            'last_name' => [$this->requiredIfPut(), 'string', 'max:255'],
+            'email' => [
+                $this->requiredIfPut(),
+                'string',
+                'email',
+                'max:255',
+                Rule::unique(User::query()->getQuery()->from)->ignore($this->route('id'))
+            ],
         ];
         return array_merge(parent::rules(), $rules);
+    }
+
+    private function requiredIfPut()
+    {
+        if ($this->getMethod() === 'PUT') {
+            return 'required';
+        } else {
+            return 'sometimes';
+        }
     }
 }
--- tests/API/Admin/UserApiTest.php
+++ tests/API/Admin/UserApiTest.php
@@ -24,7 +24,9 @@ class UserApiTest extends TestCase
         $this->response
             ->assertOk()
             ->assertJsonFragment([
-                'id' => $user->getKey()
+                'id' => $user->getKey(),
+                'email' => $user->email,
+                'first_name' => $user->first_name
             ]);
     }
 
@@ -48,5 +50,76 @@ class UserApiTest extends TestCase
         $this->response = $this->actingAs($user)->json('GET', '/api/admin/users/' . $user->id);
         $this->response
             ->assertForbidden();
+    }
+
+    public function testPatchUser()
+    {
+        /** @var User $user */
+        $user = $this->makeStudent();
+        /** @var User $admin */
+        $admin = $this->makeAdmin();
+
+        $new_first_name = $user->first_name . ' new';
+
+        $this->response = $this->actingAs($admin)->json('PATCH', '/api/admin/users/' . $user->getKey(), [
+            'first_name' => $new_first_name
+        ]);
+
+        $this->response
+            ->assertOk()
+            ->assertJsonFragment([
+                'first_name' => $new_first_name,
+                'last_name' => $user->last_name
+            ])
+            ->assertJsonMissing([
+                'first_name' => $user->first_name
+            ]);
+
+        $user->refresh();
+        $this->assertEquals($user->first_name, $new_first_name);
+    }
+
+    public function testPutUser()
+    {
+        /** @var User $user */
+        $user = $this->makeStudent();
+        /** @var User $admin */
+        $admin = $this->makeAdmin();
+
+        $new_first_name = $user->first_name . ' new';
+
+        $this->response = $this->actingAs($admin)->json('PUT', '/api/admin/users/' . $user->getKey(), [
+            'first_name' => $new_first_name,
+            'last_name' => $user->last_name,
+            'email' => $user->email,
+        ]);
+
+        $this->response
+            ->assertOk()
+            ->assertJsonFragment([
+                'first_name' => $new_first_name,
+                'last_name' => $user->last_name,
+                'email' => $user->email,
+            ])
+            ->assertJsonMissing([
+                'first_name' => $user->first_name,
+            ]);
+    }
+
+    public function testFailValidationTryingToPutUser()
+    {
+        /** @var User $user */
+        $user = $this->makeStudent();
+        /** @var User $admin */
+        $admin = $this->makeAdmin();
+
+        $new_first_name = $user->first_name . ' new';
+
+        $this->response = $this->actingAs($admin)->json('PUT', '/api/admin/users/' . $user->getKey(), [
+            'first_name' => $new_first_name
+        ]);
+
+        $this->response
+            ->assertStatus(422);
     }
 }
