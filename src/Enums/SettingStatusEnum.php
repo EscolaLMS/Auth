0d9f4df945fb4fc2e3c91f58605ebbbd88475974
Maciej Rymarz <59456825+mako321@users.noreply.github.com>
2022-01-21 12:50:27 +0100

    Change setting value type to enum (#103)

---
+++ src/Enums/SettingStatusEnum.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace EscolaLms\Auth\Enums;
+
+use EscolaLms\Core\Enums\BasicEnum;
+
+class SettingStatusEnum extends BasicEnum
+{
+    const ENABLED  = 'enabled';
+    const DISABLED = 'disabled';
+}
--- src/config.php
+++ src/config.php
@@ -1,11 +1,13 @@
 <?php
 
+use EscolaLms\Auth\Enums\SettingStatusEnum;
+
 return [
     'superadmins' => [
         env('AUTH_SUPERADMIN_EMAIL'),
     ],
     'additional_fields' => [],
     'additional_fields_required' => [],
-    'registration_enabled' => true,
-    'account_must_be_enabled_by_admin' => false,
+    'registration' => SettingStatusEnum::ENABLED,
+    'account_must_be_enabled_by_admin' => SettingStatusEnum::DISABLED,
 ];
