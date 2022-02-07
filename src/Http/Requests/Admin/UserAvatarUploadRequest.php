bb8441b5351c93ea9823d18bf797ab5cc34105ac
Maciej Rymarz <maciej.rymarz@escolasoft.com>
2022-02-07 13:00:09 +0100

    User avatar file as path

--- src/Http/Requests/Admin/UserAvatarUploadRequest.php
+++ src/Http/Requests/Admin/UserAvatarUploadRequest.php
@@ -2,7 +2,7 @@
 
 namespace EscolaLms\Auth\Http\Requests\Admin;
 
-use EscolaLms\Auth\Http\Requests\Admin\AbstractUserIdInRouteRequest;
+use EscolaLms\Files\Rules\FileOrStringRule;
 
 class UserAvatarUploadRequest extends AbstractUserIdInRouteRequest
 {
@@ -11,10 +11,12 @@ class UserAvatarUploadRequest extends AbstractUserIdInRouteRequest
         return $this->user()->can('update', $this->getRouteUser());
     }
 
-    public function rules()
+    public function rules(): array
     {
+        $pathPrefix = 'avatars/' . $this->route('id');
+
         $rules = [
-            'avatar' => ['required', 'file', 'mimes:png,jpg,jpeg,svg,gif'],
+            'avatar' => ['required', new FileOrStringRule(['file', 'mimes:png,jpg,jpeg,svg,gif'], $pathPrefix)],
         ];
         return array_merge(parent::rules(), $rules);
     }
