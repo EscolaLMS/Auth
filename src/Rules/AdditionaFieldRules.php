1382c36b8bb30094aa5621b4ad0486944fa6e400
Paweł Chołaj <pawel.cholaj@escolasoft.com>
2022-02-23 18:28:46 +0100

    Validate option_ additional fields as booleans

--- src/Rules/AdditionaFieldRules.php
+++ src/Rules/AdditionaFieldRules.php
@@ -4,6 +4,7 @@ namespace EscolaLms\Auth\Rules;
 
 use EscolaLms\Auth\EscolaLmsAuthServiceProvider;
 use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Str;
 
 class AdditionaFieldRules
 {
@@ -11,11 +12,21 @@ class AdditionaFieldRules
     public static function rules()
     {
         $rules = [];
+
         foreach (Config::get(EscolaLmsAuthServiceProvider::CONFIG_KEY . '.additional_fields', []) as $field) {
-            $rules[$field] = ['nullable', 'string'];
+            $rules[$field] = ['nullable'];
         }
+
         foreach (Config::get(EscolaLmsAuthServiceProvider::CONFIG_KEY . '.additional_fields_required', []) as $field) {
-            $rules[$field] = ['required', 'string'];
+            $rules[$field] = ['required'];
+        }
+
+        foreach ($rules as $field => $arr) {
+            if (Str::startsWith($field, ['option_'])) {
+                $rules[$field][] = 'boolean';
+            } else {
+                $rules[$field][] = 'string';
+            }
         }
 
         return $rules;
