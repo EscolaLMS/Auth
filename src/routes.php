2c5990ede99f1bbc4022788687901fd814e8a2e4
Hubert Krzysztofiak <hubert.krzysztofiak@escolasoft.com>
2022-05-05 20:02:09 +0200

    Remove custom Route

--- src/routes.php
+++ src/routes.php
@@ -10,7 +10,7 @@ use EscolaLms\Auth\Http\Controllers\LogoutApiController;
 use EscolaLms\Auth\Http\Controllers\ProfileAPIController;
 use EscolaLms\Auth\Http\Controllers\RegisterApiController;
 use EscolaLms\Auth\Http\Middleware\RegistrationEnabled;
-use EscolaLms\Core\Http\Facades\Route;
+use Illuminate\Routing\Route;
 
 Route::group(['prefix' => 'api'], function () {
     Route::group(['prefix' => 'auth'], function () {
@@ -31,15 +31,15 @@ Route::group(['prefix' => 'api'], function () {
 
         Route::group(['prefix' => 'email'], function () {
             Route::get('verify/{id}/{hash}', [AuthApiController::class, 'verifyEmail'])->name('verification.verify');
-            Route::post('resend', [AuthApiController::class, 'resendEmailVerification'])->middleware(Route::apply(['auth:api', 'throttle:6,1']))->name('verification.send');
+            Route::post('resend', [AuthApiController::class, 'resendEmailVerification'])->middleware(['auth:api', 'throttle:6,1'])->name('verification.send');
         });
 
-        Route::group(['middleware' => Route::apply(['auth:api'])], function () {
+        Route::group(['middleware' => ['auth:api']], function () {
             Route::post('logout', [LogoutApiController::class, 'logout'])->name('logout.api');
             Route::get('refresh', [AuthApiController::class, 'refresh']);
         });
     });
-    Route::middleware(Route::apply(['auth:api']))->prefix('profile')->group(function () {
+    Route::middleware(['auth:api'])->prefix('profile')->group(function () {
         Route::get('/me', [ProfileAPIController::class, 'me']);
         Route::put('/me', [ProfileAPIController::class, 'update']);
         Route::put('/me-auth', [ProfileAPIController::class, 'updateAuthData']);
@@ -51,7 +51,7 @@ Route::group(['prefix' => 'api'], function () {
         Route::delete('/delete-avatar', [ProfileAPIController::class, 'deleteAvatar']);
     });
 
-    Route::middleware(Route::apply(['auth:api']))->prefix('admin')->group(function () {
+    Route::middleware(['auth:api'])->prefix('admin')->group(function () {
         Route::group(['prefix' => 'users'], function () {
             // Users
             Route::get('/', [UserController::class, 'listUsers']);
