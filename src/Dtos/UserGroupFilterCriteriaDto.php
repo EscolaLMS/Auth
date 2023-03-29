19f824c5d51d28a6dade42db2f0a8d03e0e7d54b
Tomasz Smolarek <59400506+dyfero@users.noreply.github.com>
2023-03-29 11:27:39 +0200

    Filter user groups (#160)
    * Filter user groups
    
    * fixes
    
    * Revert "fixes"
    
    This reverts commit 8b5b1c801552043c9cc2c446c7ef1a463ee85b7b.
    
    ---------
    
    Co-authored-by: Tomasz Smolarek <tomasz.smolarek@escolasoft.com>

--- src/Dtos/UserGroupFilterCriteriaDto.php
+++ src/Dtos/UserGroupFilterCriteriaDto.php
@@ -2,12 +2,14 @@
 
 namespace EscolaLms\Auth\Dtos;
 
+use EscolaLms\Auth\Enums\AuthPermissionsEnum;
 use EscolaLms\Auth\Repositories\Criteria\UserGroupRootCriterion;
 use EscolaLms\Auth\Repositories\Criteria\UserGroupSearchCriterion;
 use EscolaLms\Core\Dtos\Contracts\DtoContract;
 use EscolaLms\Core\Dtos\Contracts\InstantiateFromRequest;
 use EscolaLms\Core\Dtos\CriteriaDto;
 use EscolaLms\Core\Repositories\Criteria\Primitives\EqualCriterion;
+use EscolaLms\Core\Repositories\Criteria\Primitives\HasCriterion;
 use Illuminate\Http\Request;
 use Illuminate\Support\Collection;
 
@@ -17,6 +19,14 @@ class UserGroupFilterCriteriaDto extends CriteriaDto implements DtoContract, Ins
     {
         $criteria = new Collection();
 
+        if (
+            $request->user()->can(AuthPermissionsEnum::USER_GROUP_LIST_SELF)
+            && !$request->user()->can(AuthPermissionsEnum::USER_GROUP_LIST)
+        ) {
+            $criteria->push(
+                new HasCriterion('users', fn ($query) => $query->where('user_id', $request->user()->getKey()))
+            );
+        }
         if ($request->has('search')) {
             $criteria->push(new UserGroupSearchCriterion($request->get('search')));
         }
