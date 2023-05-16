d0c31f3a2e00f36ecd53012407d3ff747be567b6
Maciej Rymarz <59456825+mako321@users.noreply.github.com>
2023-05-16 13:25:47 +0200

    Make UserSimpleResource extendable (#176)

--- src/Http/Resources/UserSimpleResource.php
+++ src/Http/Resources/UserSimpleResource.php
@@ -2,17 +2,22 @@
 
 namespace EscolaLms\Auth\Http\Resources;
 
+use EscolaLms\Auth\Traits\ResourceExtandable;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class UserSimpleResource extends JsonResource
 {
+    use ResourceExtandable;
+
     public function toArray($request): array
     {
-        return [
+        $fields = [
             'id' => $this->id,
             'first_name' => $this->first_name,
             'last_name' => $this->last_name,
             'email' => $this->email,
         ];
+
+        return self::apply($fields, $this);
     }
 }
