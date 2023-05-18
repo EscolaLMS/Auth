ef3c2c7b2e7d6ad720ae203f1f369ccaca3a3dc3
Tomasz Smolarek <tomasz.smolarek@escolasoft.com>
2023-05-18 16:25:22 +0200

    Make group resources extendable

--- src/Http/Resources/UserGroupDetailedResource.php
+++ src/Http/Resources/UserGroupDetailedResource.php
@@ -3,10 +3,13 @@
 namespace EscolaLms\Auth\Http\Resources;
 
 use EscolaLms\Auth\Models\Group;
+use EscolaLms\Auth\Traits\ResourceExtandable;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class UserGroupDetailedResource extends JsonResource
 {
+    use ResourceExtandable;
+
     public function __construct(Group $group)
     {
         parent::__construct($group);
@@ -19,7 +22,7 @@ class UserGroupDetailedResource extends JsonResource
 
     public function toArray($request)
     {
-        return [
+        $fields = [
             'id' => $this->getResource()->getKey(),
             'name' => $this->getResource()->name,
             'users' => UserSimpleResource::collection($this->getResource()->users),
@@ -27,5 +30,7 @@ class UserGroupDetailedResource extends JsonResource
             'registerable' => $this->getResource()->registerable,
             'name_with_breadcrumbs' => $this->getResource()->name_with_breadcrumbs,
         ];
+
+        return self::apply($fields, $this);
     }
 }
