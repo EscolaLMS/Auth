ac3a2b438996f5d4395c49f21382706eebde10e3
Witold Wiśniewski <witold.michal.wisniewski@gmail.com>
2024-07-15 14:21:46 +0200

    mysql 5.7 ?

--- src/Repositories/Criteria/UserGroupSearchCriterion.php
+++ src/Repositories/Criteria/UserGroupSearchCriterion.php
@@ -16,7 +16,42 @@ class UserGroupSearchCriterion extends Criterion
     public function apply(Builder $query): Builder
     {
         return $query->where(function (Builder $q) {
-            $like = DB::connection()->getPdo()->getAttribute(\PDO::ATTR_DRIVER_NAME) === 'pgsql' ? 'ILIKE' : 'LIKE';
+            $driver = DB::connection()->getPdo()->getAttribute(\PDO::ATTR_DRIVER_NAME);
+            $like = $driver === 'pgsql' ? 'ILIKE' : 'LIKE';
+
+
+            // to check mysql version
+            if ($driver === 'mysql') {
+                $version = DB::connection()->getPdo()->getAttribute(\PDO::ATTR_SERVER_VERSION);
+                if (version_compare($version, '5.7.0') <= 0) {
+                    $initialId = '1';
+                    $allChild = DB::select("SELECT id, name, parent_id
+                        FROM (SELECT * FROM groups
+                              ORDER BY parent_id, id) AS sorted_groups,
+                             (SELECT @pv := '$initialId') AS init
+                        WHERE FIND_IN_SET(parent_id, @pv)
+                        AND LENGTH(@pv := CONCAT(@pv, ',', id))");
+                    $ids = collect($allChild)->pluck('id')->toArray();
+
+                    $groupIds = implode(',', $ids);
+                    $fullNames = DB::select("SELECT id, name, parent_id,
+                        CONCAT_WS('. ', IFNULL((SELECT name FROM groups AS p WHERE p.id = g.parent_id), ''), g.name) AS full_name
+                        FROM groups AS g
+                        WHERE g.id IN ($groupIds)");
+
+                    $filteredGroups = collect($fullNames)->filter(function ($group) {
+                        return stripos($group->full_name, "%$this->value%") !== false;
+                    });
+
+                    if (count($filteredGroups) > 0) {
+                        $q->orWhereIn('id', $filteredGroups->pluck('id'));
+                    }
+
+                    return $q;
+                }
+
+            }
+
             $q->where('name', $like, "%$this->value%");
 
             $fullNameIds = DB::select("
@@ -35,6 +70,8 @@ class UserGroupSearchCriterion extends Criterion
             if (count($fullNameIds) > 0) {
                 $q->orWhereIn('id', collect($fullNameIds)->pluck('id')->toArray());
             }
+
+            return $q;
         });
     }
 }
